f36905d4827369f00cadb077c09b36f4bfbe20b5
Mariana <mariana.velasquez0502@gmail.com>
2022-01-15 13:04:49 -0500

    Installed angular material and angular fire. Seems right

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'read2gether',
+    appId: '1:946010915832:web:ff019fa295d9ac8e418c0d',
+    storageBucket: 'read2gether.appspot.com',
+    apiKey: 'AIzaSyDOP44OSo_qj3q362tthAkIaAfnyJxeq1w',
+    authDomain: 'read2gether.firebaseapp.com',
+    messagingSenderId: '946010915832',
+  },
   production: false
 };
 
